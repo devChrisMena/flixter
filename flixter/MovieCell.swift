1bfa3764a9394963b6333b891e09583e75b58eef
Christopher Mena <mena.christopher.student@gmail.com>
2021-02-12 20:52:19 -0500

    Completed project

---
+++ flixter/MovieCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCell.swift
+//  flixter
+//
+//  Created by Christopher Mena on 2/12/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
